af2f5831c0218700e3dccdb4e7724f11b1e10502
Budiman Rasyid <brasyid15@gmail.com>
2021-12-13 10:37:38 +0700

    Membuat list tile untuk simple, scheduled, and delete notifications

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,13 +9,35 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('Latihan Local Notifications'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlutterLogo(
+                size: Get.width * 0.3,
+              ),
+              ListTile(
+                title: Text('Simple Notifications'),
+                leading: Icon(Icons.notifications),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text('Scheduled Notifications'),
+                leading: Icon(Icons.notifications_active),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text('Delete Notifications'),
+                leading: Icon(Icons.delete),
+                onTap: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
